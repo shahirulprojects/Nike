355c9c1b8ebaffac6dcc003caab4e1ed9b93e0b2
shahirulprojects <shahiruliimalo@gmail.com>
2024-03-03 01:33:04 +0800

    feat:add page basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      {/* on extra large devices,padding 1.On wide devices padding-r.Padding-r and padding-b is a custom class that can be found in index.css file */}
+      Hero
+    </section>
+    <section className="padding">
+      {/* padding can also be found in index.css file */}
+      PopularProducts
+    </section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">SpecialOffer</section>
+    <section className="bg-pale-blue padding">CustomerReviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+  </main>
+);
+
+export default App;
