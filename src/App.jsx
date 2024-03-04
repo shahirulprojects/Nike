92b806916514342db236df0fdfbc941c80ebd3c8
shahirulprojects <shahiruliimalo@gmail.com>
2024-03-04 20:26:08 +0800

    feat:add components sections

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
   CustomerReviews,
   Subscribe,
   Footer,
-} from "./sections";
+} from "./sections"; // kita boleh import camni sebab kita dah export semua ni kat index.js file
 
 const App = () => (
   <main className="relative">
