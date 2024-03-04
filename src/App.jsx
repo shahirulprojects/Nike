412c4b10d112023ad82a5cf22b4b6de27f129d13
shahirulprojects <shahiruliimalo@gmail.com>
2024-03-04 20:24:40 +0800

    feat:add components sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./sections";
+
 const App = () => (
   <main className="relative">
-    Nav
+    {/* <Nav/> */}
     <section className="xl:padding-1 wide:padding-r padding-b">
       {/* on extra large devices,padding 1.On wide devices padding-r.Padding-r and padding-b is a custom class that can be found in index.css file */}
-      Hero
+      <Hero />
     </section>
     <section className="padding">
       {/* padding can also be found in index.css file */}
-      PopularProducts
+      <PopularProducts />
     </section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">SpecialOffer</section>
-    <section className="bg-pale-blue padding">CustomerReviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
   </main>
 );
 
---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+// yang ni guna so that kita akan ada satu import je dekat App.jsx tu (untuk macam nampak lagi kemasla)
+
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+};
