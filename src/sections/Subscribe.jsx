8f7496e7fa6f9775504c1f2cdb667f44407bef9d
shahirulprojects <shahiruliimalo@gmail.com>
2024-03-15 23:22:20 +0800

    styles:add basic styles to subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,5 @@
 const Subscribe = () => {
-  return <div className="bg-blue-700 text-white text-bold">Subscribe</div>;
+  return <div className="bg-blue-700 text-white">Subscribe</div>;
 };
 
 export default Subscribe;
