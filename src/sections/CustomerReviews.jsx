dd526b0ee3ea534ccc5a6d8c9dc902a53352dc3e
shahirulprojects <shahiruliimalo@gmail.com>
2024-03-19 22:46:23 +0800

    styles:change styles for customer review section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,5 @@
 const CustomerReviews = () => {
-  return (
-    <div className="bg-blue-700 text-white text-bold">CustomerReviews</div>
-  );
+  return <div className="bg-blue-700 text-white">CustomerReviews</div>;
 };
 
 export default CustomerReviews;
