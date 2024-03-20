0bdf08d670c1d43e514c2576a69c43139cc08fc5
shahirulprojects <shahiruliimalo@gmail.com>
2024-03-20 23:08:04 +0800

    styles:change styles for footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className="bg-blue-700 text-white text-bold">Footer</div>;
+  return <div className="bg-blue-700 text-white">Footer</div>;
 };
 
 export default Footer;
