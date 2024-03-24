14da88cd7b0b5c3981a5111f2cb8127461d34dea
shahirulprojects <shahiruliimalo@gmail.com>
2024-03-24 18:14:42 +0800

    docs:add comment

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      {/* min-h-screen means that it will take the entire height of the screen, w- full means that that it will take the entire width of the screen,max-container is a custom class */}
+      {/* min-h-screen means that it will take the entire height of the screen, w-full means that that it will take the entire width of the screen,max-container is a custom class */}
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p>Our Summer Collection</p>
         <h1>
