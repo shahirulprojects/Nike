b03632adb573d154cf0eeafbf2a918a339a410e3
shahirulprojects <shahiruliimalo@gmail.com>
2024-03-29 17:33:29 +0800

    docs:add comment

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -4,7 +4,7 @@ import { products } from "../constants";
 const PopularProducts = () => {
   return (
     <section id="products" className="max-container max-sm:mt-12">
-      {/* the id is for scrolling,like when the user click the popular products text,it will scroll to the popular products section */}
+      {/* the id is for scrolling,like when the user click the "popular products" text,it will scroll to the popular products section */}
       <div className="flex flex-col justify-start gap-5">
         <h2 className="text-4xl font-palanquin font-bold">
           Our <span className="text-coral-red">Popular</span> Products
