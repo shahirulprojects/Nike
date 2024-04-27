ed0d32369d0c0d97e34d19c39b0a27c8a7776bcb
shahirulprojects <shahiruliimalo@gmail.com>
2024-04-27 22:22:00 +0800

    styles:change styles for special offer section

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,5 +1,5 @@
 const SpecialOffer = () => {
-  return <div className="bg-red-900">SpecialOffer</div>;
+  return <div className="bg-red-900 text-white">SpecialOffer</div>;
 };
 
 export default SpecialOffer;
