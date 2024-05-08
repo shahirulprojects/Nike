f8236331c716c8492e9979f2cec6baf3b1dbb695
shahirulprojects <shahiruliimalo@gmail.com>
2024-05-08 23:08:52 +0800

    styles:add basic styles to special offer section

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,5 +1,5 @@
 const SpecialOffer = () => {
-  return <div className="bg-green-200">SpecialOffer</div>;
+  return <div className="bg-green-200 text-black">SpecialOffer</div>;
 };
 
 export default SpecialOffer;
